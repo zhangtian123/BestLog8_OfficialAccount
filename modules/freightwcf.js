5d7166cfa60644a95306f58b0cfd18e2366675b3
MadgeLin <1436140682@qq.com>
2018-03-29 16:36:04 +0800

    生成报价and分享

--- modules/freightwcf.js
+++ modules/freightwcf.js
@@ -5,144 +5,175 @@
 
 var Basic = require('./common');
 var xml2js = require('xml2js');
-var xmlParser = new xml2js.Parser({explicitArray : false, ignoreAttrs : true});
+var xmlParser = new xml2js.Parser({
+	explicitArray: false,
+	ignoreAttrs: true
+});
 //获取港口列表
-exports.getPorts = function(predicate, values, iCounts, callback){
+exports.getPorts = function(predicate, values, iCounts, callback) {
 	var addstring = '<WebBll_GetPorts xmlns="http://tempuri.org/">' +
-	                       '<predicate>'+predicate+'</predicate>' +
-	                       '<values>'+values+'</values>' +
-	                       '<iCounts>'+iCounts+'</iCounts>' +
-	                     '</WebBll_GetPorts>';
-    var messagestring = "http://tempuri.org/IWebAPIService/WebBll_GetPorts";
-    Basic.BasicConnect(addstring, messagestring,function(response){
-    	xmlParser.parseString(response, function (err, result) {
-			var str1=result["s:Envelope"]["s:Body"];
+		'<predicate>' + predicate + '</predicate>' +
+		'<values>' + values + '</values>' +
+		'<iCounts>' + iCounts + '</iCounts>' +
+		'</WebBll_GetPorts>';
+	var messagestring = "http://tempuri.org/IWebAPIService/WebBll_GetPorts";
+	Basic.BasicConnect(addstring, messagestring, function(response) {
+		xmlParser.parseString(response, function(err, result) {
+			var str1 = result["s:Envelope"]["s:Body"];
 			var str2 = str1.WebBll_GetPortsResponse.WebBll_GetPortsResult;
-			var array=[];
+			var array = [];
 			str1 = JSON.parse(str2);
 			//console.log(str1[0].OriginalValues);
-			for(var i=0;i<str1.length;i++)
-			{
-				var str3 = str1[i];//.OriginalValues;
-				var str4 = {NAMECH:str3.NAMECH,NAMEEN:str3.NAMEEN};
+			for(var i = 0; i < str1.length; i++) {
+				var str3 = str1[i]; //.OriginalValues;
+				var str4 = {
+					NAMECH: str3.NAMECH,
+					NAMEEN: str3.NAMEEN
+				};
 				//console.log(str4);
 				array.push(str4);
 			}
 			callback(array);
 		});
-    })
+	})
 }
 
 //获取单位列表
-exports.getPackage = function(predicate, values, iCounts, callback){
+exports.getPackage = function(predicate, values, iCounts, callback) {
 	var addstring = '<WebBll_GetPackageType xmlns="http://tempuri.org/">' +
-	                       '<predicate>'+predicate+'</predicate>' +
-	                       '<values>'+values+'</values>' +
-	                       '<iCounts>'+iCounts+'</iCounts>' +
-	                     '</WebBll_GetPackageType>';
-    var messagestring = "http://tempuri.org/IWebAPIService/WebBll_GetPackageType";
-    Basic.BasicConnect(addstring, messagestring,function(response){
-    	xmlParser.parseString(response, function (err, result) {
-			var str1=result["s:Envelope"]["s:Body"];
+		'<predicate>' + predicate + '</predicate>' +
+		'<values>' + values + '</values>' +
+		'<iCounts>' + iCounts + '</iCounts>' +
+		'</WebBll_GetPackageType>';
+	var messagestring = "http://tempuri.org/IWebAPIService/WebBll_GetPackageType";
+	Basic.BasicConnect(addstring, messagestring, function(response) {
+		xmlParser.parseString(response, function(err, result) {
+			var str1 = result["s:Envelope"]["s:Body"];
 			var str2 = str1.WebBll_GetPackageTypeResponse.WebBll_GetPackageTypeResult;
-			var array=[];
+			var array = [];
 			str1 = JSON.parse(str2);
 			//console.log(str1[0].OriginalValues);
-			for(var i=0;i<str1.length;i++)
-			{
+			for(var i = 0; i < str1.length; i++) {
 				var str3 = str1[i];
 				//var str4 = {PACKAGETYPEID:str3.PACKAGETYPEID,TYPECODE:str3.TYPECODE,TYPENAMECH:str3.TYPENAMECH,TYPENAMEEN:str3.TYPENAMEEN};
 				//console.log(str4);
-				var str4 = {text:str3.TYPENAMECH,value:str3.TYPENAMEEN};
+				var str4 = {
+					text: str3.TYPENAMECH,
+					value: str3.TYPENAMEEN
+				};
 				array.push(str4);
 			}
 			callback(array);
 		});
-    })
+	})
 }
 
 //获取运输方案列表
-exports.getFCLList = function(predicate, values, iCounts, callback){
+exports.getFCLList = function(predicate, values, iCounts, callback) {
 	var addstring = '<GetQuoteFCLList xmlns="http://tempuri.org/">' +
-	                       '<predicate>'+predicate+'</predicate>' +
-	                       '<values>'+values+'</values>' +
-	                       '<numStr>'+iCounts+'</numStr>' +
-	                     '</GetQuoteFCLList>';
-    var messagestring = "http://tempuri.org/IWebAPIService/GetQuoteFCLList";
-    Basic.BasicConnect(addstring, messagestring,function(response){
-    	//console.log(response);
-    	xmlParser.parseString(response, function (err, result) {
-			var str1=result["s:Envelope"]["s:Body"];
+		'<predicate>' + predicate + '</predicate>' +
+		'<values>' + values + '</values>' +
+		'<numStr>' + iCounts + '</numStr>' +
+		'</GetQuoteFCLList>';
+	var messagestring = "http://tempuri.org/IWebAPIService/GetQuoteFCLList";
+	Basic.BasicConnect(addstring, messagestring, function(response) {
+		//console.log(response);
+		xmlParser.parseString(response, function(err, result) {
+			var str1 = result["s:Envelope"]["s:Body"];
 			var status = str1.GetQuoteFCLListResponse.GetQuoteFCLListResult;
 			str1 = JSON.parse(status);
 			//console.log(status);
-			var array=[];
-			var count = {COUNT:str1.length};
+			var array = [];
+			var count = {
+				COUNT: str1.length
+			};
 			array.push(count);
-			if(str1.length!=0)
-			{
-				for(var i=0;i<str1.length;i++)
-				{
-					var str3 = str1[i];//.OriginalValues;
-					var str4 = {BILLID:str3.BILLID,BILLNO:str3.BILLNO,CUSTOMER:str3.CARRIER,
-						SAILINGDATE:str3.SAILINGDATE,DAYNUM:str3.DAYNUM,PORTLOADING:str3.PORTLOADING,
-						PORTDISCHARGE:str3.PORTDISCHARGE,ISDIRECT:str3.ISDIRECT,BASEPORT:str3.BASEPORT,
-						STARTDATE:str3.STARTDATE,ENDDATE:str3.ENDDATE,PRICE20GP:str3.BossPrice20GP,
-						PRICE40GP:str3.BossPrice40GP,PRICE40HQ:str3.BossPrice40HQ,TotalPrice:str3.TotalPrice};
+			if(str1.length != 0) {
+				for(var i = 0; i < str1.length; i++) {
+					var str3 = str1[i]; //.OriginalValues;
+					var str4 = {
+						BILLID: str3.BILLID,
+						BILLNO: str3.BILLNO,
+						CUSTOMER: str3.CARRIER,
+						SAILINGDATE: str3.SAILINGDATE,
+						DAYNUM: str3.DAYNUM,
+						PORTLOADING: str3.PORTLOADING,
+						PORTDISCHARGE: str3.PORTDISCHARGE,
+						ISDIRECT: str3.ISDIRECT,
+						BASEPORT: str3.BASEPORT,
+						STARTDATE: str3.STARTDATE,
+						ENDDATE: str3.ENDDATE,
+						PRICE20GP: str3.BossPrice20GP,
+						PRICE40GP: str3.BossPrice40GP,
+						PRICE40HQ: str3.BossPrice40HQ,
+						TotalPrice: str3.TotalPrice
+					};
 					//console.log(str4);
 					array.push(str4);
 				}
 			}
 			callback(array);
 		});
-    })
+	})
 }
-exports.SetCrQuoteReportSea = function(quote,detail,callback){
+exports.SetCrQuoteReportSea = function(quote, detail, callback) {
 	var addstring = '<CrQuoteReportSeaBll_SetCrQuoteReportSeaByApp xmlns="http://tempuri.org/">' +
-	                       '<quote>'+quote+'</quote>' +
-	                       '<detail>'+detail+'</detail>' +
-	                     '</CrQuoteReportSeaBll_SetCrQuoteReportSeaByApp>';
-    var messagestring = "http://tempuri.org/IWebAPIService/CrQuoteReportSeaBll_SetCrQuoteReportSeaByApp";
-    Basic.BasicConnect(addstring, messagestring,function(response){
-		xmlParser.parseString(response, function (err, result) {
-    		console.log(response);
-			var str1=result["s:Envelope"]["s:Body"];
+		'<quote>' + quote + '</quote>' +
+		'<detail>' + detail + '</detail>' +
+		'</CrQuoteReportSeaBll_SetCrQuoteReportSeaByApp>';
+	var messagestring = "http://tempuri.org/IWebAPIService/CrQuoteReportSeaBll_SetCrQuoteReportSeaByApp";
+	Basic.BasicConnect(addstring, messagestring, function(response) {
+		xmlParser.parseString(response, function(err, result) {
+			console.log(response);
+			var str1 = result["s:Envelope"]["s:Body"];
 			var status = str1.CrQuoteReportSeaBll_SetCrQuoteReportSeaByAppResponse.CrQuoteReportSeaBll_SetCrQuoteReportSeaByAppResult;
 			callback(status);
 		});
-    })
+	})
 }
-exports.getQuoteList = function(predicate, values, callback){
+exports.getQuoteList = function(predicate, values, callback) {
 	var addstring = '<CrQuoteReportSeaBll_GetCrQuoteReportSeaList xmlns="http://tempuri.org/">' +
-	                       '<predicate>'+predicate+'</predicate>' +
-	                       '<values>'+values+'</values>' +
-	                     '</CrQuoteReportSeaBll_GetCrQuoteReportSeaList>';
-    var messagestring = "http://tempuri.org/IWebAPIService/CrQuoteReportSeaBll_GetCrQuoteReportSeaList";
-    Basic.BasicConnect(addstring, messagestring,function(response){
-    	//console.log(response);
-    	xmlParser.parseString(response, function (err, result) {
-			var str1=result["s:Envelope"]["s:Body"];
+		'<predicate>' + predicate + '</predicate>' +
+		'<values>' + values + '</values>' +
+		'</CrQuoteReportSeaBll_GetCrQuoteReportSeaList>';
+	var messagestring = "http://tempuri.org/IWebAPIService/CrQuoteReportSeaBll_GetCrQuoteReportSeaList";
+	Basic.BasicConnect(addstring, messagestring, function(response) {
+		//console.log(response);
+		xmlParser.parseString(response, function(err, result) {
+			var str1 = result["s:Envelope"]["s:Body"];
 			var status = str1.CrQuoteReportSeaBll_GetCrQuoteReportSeaListResponse.CrQuoteReportSeaBll_GetCrQuoteReportSeaListResult;
 			str1 = JSON.parse(status);
 			//console.log(status);
-			var array=[];
-			var count = {COUNT:str1.length};
+			var array = [];
+			var count = {
+				COUNT: str1.length
+			};
 			array.push(count);
-			if(str1.length!=0)
-			{
-				for(var i=0;i<str1.length;i++)
-				{
-					var str3 = str1[i];//.OriginalValues;
-					var str4 = {CUSTOMER:str3.Customer,BEGINDATE:str3.BeginDate,ENDDATE:str3.EndDate,
-						CARRIER:str3.CARRIER,PORTLOADING:str3.PORTLOADING,PORTDISCHARGE:str3.PORTDISCHARGE,
-						PRICE20GP:str3.PRICE20GP,PRICE40GP:str3.PRICE40GP,PRICE40HQ:str3.PRICE40HQ,
-						QUANTITY20GP:str3.QUANTITY20GP,QUANTITY40GP:str3.QUANTITY40GP,QUANTITY40HQ:str3.QUANTITY40HQ,
-					ISDIRECT:str3.ISDIRECT,DAYNUM:str3.DAYNUM,ETD:str3.ETD};
+			if(str1.length != 0) {
+				for(var i = 0; i < str1.length; i++) {
+					var str3 = str1[i]; //.OriginalValues;
+					var str4 = {
+						CUSTOMER: str3.Customer,
+						BEGINDATE: str3.BeginDate,
+						ENDDATE: str3.EndDate,
+						CARRIER: str3.CARRIER,
+						PORTLOADING: str3.PORTLOADING,
+						PORTDISCHARGE: str3.PORTDISCHARGE,
+						PRICE20GP: str3.PRICE20GP,
+						PRICE40GP: str3.PRICE40GP,
+						PRICE40HQ: str3.PRICE40HQ,
+						QUANTITY20GP: str3.QUANTITY20GP,
+						QUANTITY40GP: str3.QUANTITY40GP,
+						QUANTITY40HQ: str3.QUANTITY40HQ,
+						ISDIRECT: str3.ISDIRECT,
+						DAYNUM: str3.DAYNUM,
+						ETD: str3.ETD
+					};
 					//console.log(str4);
 					array.push(str4);
 				}
 			}
 			callback(array);
 		});
-    })
+	})
 }
--- modules/trailerwcf.js
+++ modules/trailerwcf.js
@@ -5,164 +5,176 @@
 
 var Basic = require('./common');
 var xml2js = require('xml2js');
-var xmlParser = new xml2js.Parser({explicitArray : false, ignoreAttrs : true});
-var flag=0;
-var aflag=0;
+var xmlParser = new xml2js.Parser({
+	explicitArray: false,
+	ignoreAttrs: true
+});
+var flag = 0;
+var aflag = 0;
 //获取节点流
-var trailerDetail = function(BillID,num, callback){
+var trailerDetail = function(BillID, num, callback) {
 	var addstring = '<CommonBll_GetNodeViewInfos xmlns="http://tempuri.org/">' +
-	                       '<BillID>'+BillID+'</BillID>' +
-	                     '</CommonBll_GetNodeViewInfos>';
-    var messagestring = "http://tempuri.org/IWebAPIService/CommonBll_GetNodeViewInfos";
-    Basic.BasicConnect(addstring, messagestring,function(response){
-    	//console.log(response);
-    	flag=num;
-    	xmlParser.parseString(response, function (err, result) {
-			var str1=result["s:Envelope"]["s:Body"];
+		'<BillID>' + BillID + '</BillID>' +
+		'</CommonBll_GetNodeViewInfos>';
+	var messagestring = "http://tempuri.org/IWebAPIService/CommonBll_GetNodeViewInfos";
+	Basic.BasicConnect(addstring, messagestring, function(response) {
+		//console.log(response);
+		flag = num;
+		xmlParser.parseString(response, function(err, result) {
+			var str1 = result["s:Envelope"]["s:Body"];
 			var status = str1.CommonBll_GetNodeViewInfosResponse.CommonBll_GetNodeViewInfosResult;
 			str1 = JSON.parse(status);
-			var array=[];
-			for(var i=0;i<str1.length;i++)
-			{
+			var array = [];
+			for(var i = 0; i < str1.length; i++) {
 				var nodecode = str1[i].NODECODE;
-				if(str1[i].IsFinished && (nodecode == 'FMS_TCMX05' || nodecode== 'FMS_TCMX06' || nodecode== 'FMS_TCMX07' 
-				   || nodecode== 'FMS_TCMX08' || nodecode== 'FMS_TCMX09'
-				   || nodecode== 'FMS_TCMX10' || nodecode== 'FMS_TCMX11'))
-				{
-					var str4 = {NODECODE:str1[i].NODECODE,FINISHEDDATE:str1[i].FINISHEDDATE};
+				if(str1[i].IsFinished && (nodecode == 'FMS_TCMX05' || nodecode == 'FMS_TCMX06' || nodecode == 'FMS_TCMX07' ||
+						nodecode == 'FMS_TCMX08' || nodecode == 'FMS_TCMX09' ||
+						nodecode == 'FMS_TCMX10' || nodecode == 'FMS_TCMX11')) {
+					var str4 = {
+						NODECODE: str1[i].NODECODE,
+						FINISHEDDATE: str1[i].FINISHEDDATE
+					};
 					array.push(str4);
 				}
 			}
 			callback(array);
 		});
-    })
+	})
 }
 //获取派车单列表
-exports.carOrderList = function(predicate, values, pageNum, pageSize, orderByProperty, rowsCount, callback){
-	flag=0;
-	aflag=0;
+exports.carOrderList = function(predicate, values, pageNum, pageSize, orderByProperty, rowsCount, callback) {
+	flag = 0;
+	aflag = 0;
 	var addstring = '<FWDispatchBll_GetDisPatchList xmlns="http://tempuri.org/">' +
-	                       '<predicate>'+predicate+'</predicate>' +
-	                       '<values>'+values+'</values>' +
-	                       '<pageNum>'+pageNum+'</pageNum>' +
-	                       '<pageSize>'+pageSize+'</pageSize>' +
-	                       '<orderByProperty>'+orderByProperty+'</orderByProperty>' +
-	                       '<rowsCount>'+rowsCount+'</rowsCount>' +
-	                     '</FWDispatchBll_GetDisPatchList>';
- //   var messagestring = "http://tempuri.org/IWebAPIService/FWDispatchBll_GetDisPatchList";
- 	var messagestring = "http://tempuri.org/IWebAPIService/FWDispatchBll_GetDisPatchList";
-    Basic.BasicConnect(addstring, messagestring,function(response){
-    	//callback(response);
-    	var arraystr=[];
-		xmlParser.parseString(response, function (err, result) {
-			var str1=result["s:Envelope"]["s:Body"];
+		'<predicate>' + predicate + '</predicate>' +
+		'<values>' + values + '</values>' +
+		'<pageNum>' + pageNum + '</pageNum>' +
+		'<pageSize>' + pageSize + '</pageSize>' +
+		'<orderByProperty>' + orderByProperty + '</orderByProperty>' +
+		'<rowsCount>' + rowsCount + '</rowsCount>' +
+		'</FWDispatchBll_GetDisPatchList>';
+	//   var messagestring = "http://tempuri.org/IWebAPIService/FWDispatchBll_GetDisPatchList";
+	var messagestring = "http://tempuri.org/IWebAPIService/FWDispatchBll_GetDisPatchList";
+	Basic.BasicConnect(addstring, messagestring, function(response) {
+		//callback(response);
+		var arraystr = [];
+		xmlParser.parseString(response, function(err, result) {
+			var str1 = result["s:Envelope"]["s:Body"];
 			var status = str1.FWDispatchBll_GetDisPatchListResponse.rowsCount;
-			var array=[];
-			var count = {COUNT:status};
+			var array = [];
+			var count = {
+				COUNT: status
+			};
 			array.push(count);
-			if(status!=0)
-			{
+			if(status != 0) {
 				var str2 = str1.FWDispatchBll_GetDisPatchListResponse.FWDispatchBll_GetDisPatchListResult;
 				str1 = JSON.parse(str2);
 				//console.log(str1[0].OriginalValues);
-				for(var i=0;i<str1.length;i++)
-				{
-					var str3 = str1[i];//.OriginalValues;
+				for(var i = 0; i < str1.length; i++) {
+					var str3 = str1[i]; //.OriginalValues;
 					arraystr.push(str3);
-					if(i<str1.length)
-					{
-						trailerDetail(str3.BILLID,i,function(result){
-							if(arraystr[flag])
-							{
-								var str4 = {BILLID:arraystr[flag].BILLID,BILLNO:arraystr[flag].BILLNO,BILLCODE:arraystr[flag].BILLCODE,CONTAINERNO:arraystr[flag].CONTAINERNO,SIZE:arraystr[flag].SIZE,TYPE:arraystr[flag].TYPE,TRUCKNO:arraystr[flag].TRUCKNO,DRIVER:arraystr[flag].DRIVER,TELEPHONE:arraystr[flag].TELEPHONE,DETAIL:result};
+					if(i < str1.length) {
+						trailerDetail(str3.BILLID, i, function(result) {
+							if(arraystr[flag]) {
+								var str4 = {
+									BILLID: arraystr[flag].BILLID,
+									BILLNO: arraystr[flag].BILLNO,
+									BILLCODE: arraystr[flag].BILLCODE,
+									CONTAINERNO: arraystr[flag].CONTAINERNO,
+									SIZE: arraystr[flag].SIZE,
+									TYPE: arraystr[flag].TYPE,
+									TRUCKNO: arraystr[flag].TRUCKNO,
+									DRIVER: arraystr[flag].DRIVER,
+									TELEPHONE: arraystr[flag].TELEPHONE,
+									DETAIL: result
+								};
 								array.push(str4);
 							}
 							aflag++;
-							if(aflag==str1.length)
-							{
+							if(aflag == str1.length) {
 								callback(array);
 							}
 						})
 					}
-					
+
 				}
 			}
 		});
-    })
+	})
 }
 
-exports.trailerFinish = function(Dispatch,NodeCode,UserCode,DocumentList,callback){
+exports.trailerFinish = function(Dispatch, NodeCode, UserCode, DocumentList, callback) {
 	var addstring = '<FWDispatchBll_DispatchNodeFinish xmlns="http://tempuri.org/">' +
-	                       '<Trailer>'+Dispatch+'</Trailer>' +
-	                       '<NodeCode>'+NodeCode+'</NodeCode>' +
-	                       '<UserCode>'+UserCode+'</UserCode>' +
-	                       '<DocumentList>'+DocumentList+'</DocumentList>' +
-	                     '</FWDispatchBll_DispatchNodeFinish>';
-    var messagestring = "http://tempuri.org/IWebAPIService/FWDispatchBll_DispatchNodeFinish";
-    Basic.BasicConnect(addstring, messagestring,function(response){
+		'<Trailer>' + Dispatch + '</Trailer>' +
+		'<NodeCode>' + NodeCode + '</NodeCode>' +
+		'<UserCode>' + UserCode + '</UserCode>' +
+		'<DocumentList>' + DocumentList + '</DocumentList>' +
+		'</FWDispatchBll_DispatchNodeFinish>';
+	var messagestring = "http://tempuri.org/IWebAPIService/FWDispatchBll_DispatchNodeFinish";
+	Basic.BasicConnect(addstring, messagestring, function(response) {
 		console.log(response);
-		xmlParser.parseString(response, function (err, result) {
-    		console.log(response);
-			var str1=result["s:Envelope"]["s:Body"];
+		xmlParser.parseString(response, function(err, result) {
+			console.log(response);
+			var str1 = result["s:Envelope"]["s:Body"];
 			var status = str1.FWDispatchBll_DispatchNodeFinishResponse.FWDispatchBll_DispatchNodeFinishResult;
 			str1 = JSON.parse(status);
 			callback(str1.Status);
 		});
-    })
+	})
 }
 
 /* 获取货物件数单位列表*/
-exports.trailerPackageType = function(predicate, values, iCounts,callback){
+exports.trailerPackageType = function(predicate, values, iCounts, callback) {
 	var addstring = '<WebBll_GetPackageType xmlns="http://tempuri.org/">' +
-	                       '<predicate>'+predicate+'</predicate>' +
-	                       '<values>'+values+'</values>' +
-	                       '<iCounts>'+iCounts+'</iCounts>' +
-	                     '</WebBll_GetPackageType>';
-    var messagestring = "http://tempuri.org/IWebAPIService/WebBll_GetPackageType";
-    Basic.BasicConnect(addstring, messagestring,function(response){
+		'<predicate>' + predicate + '</predicate>' +
+		'<values>' + values + '</values>' +
+		'<iCounts>' + iCounts + '</iCounts>' +
+		'</WebBll_GetPackageType>';
+	var messagestring = "http://tempuri.org/IWebAPIService/WebBll_GetPackageType";
+	Basic.BasicConnect(addstring, messagestring, function(response) {
 		console.log(response);
-//		xmlParser.parseString(response, function (err, result) {
-//  		console.log(response);
-//			var str1=result["s:Envelope"]["s:Body"];
-//			var status = str1.FWDeclarationBll_DeclarationNodeFinishResponse.FWDeclarationBll_DeclarationNodeFinishResult;
-//			str1 = JSON.parse(status);
-//			callback(str1.Status);
-//		});
-    })
+		//		xmlParser.parseString(response, function (err, result) {
+		//  		console.log(response);
+		//			var str1=result["s:Envelope"]["s:Body"];
+		//			var status = str1.FWDeclarationBll_DeclarationNodeFinishResponse.FWDeclarationBll_DeclarationNodeFinishResult;
+		//			str1 = JSON.parse(status);
+		//			callback(str1.Status);
+		//		});
+	})
 }
 
 /* 获取司机列表 Supier:所属公司*/
-exports.trailerGetDrivers = function(Suplier,callback){
+exports.trailerGetDrivers = function(Suplier, callback) {
 	var addstring = '<FWDispatchBll_GetDrivers xmlns="http://tempuri.org/">' +
-	                       '<Suplier>'+Suplier+'</Suplier>' +
-	                     '</FWDispatchBll_GetDrivers>';
-    var messagestring = "http://tempuri.org/IWebAPIService/FWDispatchBll_GetDrivers";
-    Basic.BasicConnect(addstring, messagestring,function(response){
+		'<Suplier>' + Suplier + '</Suplier>' +
+		'</FWDispatchBll_GetDrivers>';
+	var messagestring = "http://tempuri.org/IWebAPIService/FWDispatchBll_GetDrivers";
+	Basic.BasicConnect(addstring, messagestring, function(response) {
 		console.log(response);
-//		xmlParser.parseString(response, function (err, result) {
-//  		console.log(response);
-//			var str1=result["s:Envelope"]["s:Body"];
-//			var status = str1.FWDeclarationBll_DeclarationNodeFinishResponse.FWDeclarationBll_DeclarationNodeFinishResult;
-//			str1 = JSON.parse(status);
-//			callback(str1.Status);
-//		});
-    })
+		//		xmlParser.parseString(response, function (err, result) {
+		//  		console.log(response);
+		//			var str1=result["s:Envelope"]["s:Body"];
+		//			var status = str1.FWDeclarationBll_DeclarationNodeFinishResponse.FWDeclarationBll_DeclarationNodeFinishResult;
+		//			str1 = JSON.parse(status);
+		//			callback(str1.Status);
+		//		});
+	})
 }
 
 /* 获取车号列表 Supier:所属公司*/
-exports.trailerGetTruck = function(Suplier,callback){
+exports.trailerGetTruck = function(Suplier, callback) {
 	var addstring = '<FWDispatchBll_GetTruckList xmlns="http://tempuri.org/">' +
-	                       '<Suplier>'+Suplier+'</Suplier>' +
-	                     '</FWDispatchBll_GetTruckList>';
-    var messagestring = "http://tempuri.org/IWebAPIService/FWDispatchBll_GetTruckList";
-    Basic.BasicConnect(addstring, messagestring,function(response){
+		'<Suplier>' + Suplier + '</Suplier>' +
+		'</FWDispatchBll_GetTruckList>';
+	var messagestring = "http://tempuri.org/IWebAPIService/FWDispatchBll_GetTruckList";
+	Basic.BasicConnect(addstring, messagestring, function(response) {
 		console.log(response);
-//		xmlParser.parseString(response, function (err, result) {
-//  		console.log(response);
-//			var str1=result["s:Envelope"]["s:Body"];
-//			var status = str1.FWDeclarationBll_DeclarationNodeFinishResponse.FWDeclarationBll_DeclarationNodeFinishResult;
-//			str1 = JSON.parse(status);
-//			callback(str1.Status);
-//		});
-    })
+		//		xmlParser.parseString(response, function (err, result) {
+		//  		console.log(response);
+		//			var str1=result["s:Envelope"]["s:Body"];
+		//			var status = str1.FWDeclarationBll_DeclarationNodeFinishResponse.FWDeclarationBll_DeclarationNodeFinishResult;
+		//			str1 = JSON.parse(status);
+		//			callback(str1.Status);
+		//		});
+	})
 }
