fb289a953934b4a8017c6c4652ffc6a24d240508
ZZYHHH <71880171@qq.com>
2018-04-10 11:39:22 +0800

    运价查询分享样式修改

--- public/javascripts/shareFreightQuery.js
+++ public/javascripts/shareFreightQuery.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
 						transtype = "非直达";
 					}
 					var oDiv = document.createElement('div');
-					oDiv.className = 'mui-card content-item';
+					oDiv.id = 'card';
 					Inner(index, value.BILLID, value.PORTLOADING, transtype, value.PORTDISCHARGE, value.BASEPORT, value.CUSTOMER, value.SAILINGDATE, value.DAYNUM, value.STARTDATE, value.ENDDATE, value.PRICE20GP, value.PRICE40GP, value.PRICE40HQ, oDiv,value.TotalPrice);
 					aDiv.appendChild(oDiv);
 					index++;
@@ -34,38 +34,33 @@ $(document).ready(function() {
 
 var Inner = function(index, id, begin, line, end, port, company, time, sail, expirydatebegin, expirydateend, GPprice20, GPprice40, HCprice40, oDiv,TotalPrice) {
 	oDiv.innerHTML = oDiv.innerHTML +
-		'<div class="toplabel"></div>' +
-		'<!--页眉，放置标题-->' +
-		'<div class="mui-card-header">' +
-		'<span class="left-text">' + begin + '-' + line + '-' + end + '</span>' +
-		'<span class="right-text">' + (port == null ? '' : port) + '</span>' +
-		'</div>' +
-		'<!--内容区-->' +
-		'<div class="mui-card-content">';
-	if(index == 0)
-		oDiv.innerHTML = oDiv.innerHTML + '<div class="mark-benefit"></div>';
-	else if(index == 1)
-		oDiv.innerHTML = oDiv.innerHTML + '<div class="mark-fast"></div>';
-	oDiv.innerHTML = oDiv.innerHTML +
-		'<div class="item"  style="margin-left:5px;">' +
-		'<div class="img1"></div>' +
-		'<span>船公司/船期/航程：</span>' +
-		'<span>' + (company == null ? '' : company) + '/' + (time == null ? '' : time) + '/' + (sail == null ? '' : sail) + '</span>' +
-		'</div>' +
-		'<div class="item"  style="margin-left:5px;">' +
-		'<div class="img2"></div>' +
-		'<span>有效期：</span>' +
-		'<span>' + (expirydatebegin == null ? '' : expirydatebegin.replace('T', ' ').substr(0, expirydatebegin.length - 3)) + '至' + (expirydateend == null ? '' : expirydateend.replace('T', ' ').substr(0, expirydateend.length - 3)) + '</span>' +
-		'</div>' +
-		'<div class="item"  style="margin-left:5px;">' +
-		'<div class="img3"></div>' +
-		'<span>20GP/40GP/40HC：</span>' +
-		'<span>' + (GPprice20 == null ? '' : GPprice20) + '/' + (GPprice40 == null ? '' : GPprice40) + '/' + (HCprice40 == null ? '' : HCprice40) + '</span>' +
-		'</div>' +
-		'<div class="item"  style="margin-left:5px;">' +
-		'<div class="img4"></div>' +
-		'<span>运费：</span>' +
-		'<span>'+TotalPrice+'</span>' +
-		'</div>' +
-		'</div>';
-}
+	'<div class="item">' +
+	'<span class="left-text" id="blue-text">' + begin + '-' + line + '-' + end + '</span>' +
+	'<span class="right-text" id="blue-text">' + (port == null ? '' : port) + '</span>' +
+	'</div>' +
+	'<div class="item">'+
+	'船公司<span id="blue-text">'+(company==null ? '' : company)+
+	'</span>船期<span id="blue-text">'+(time==null ? '' : time)+
+	'</span>航程<span id="blue-text">'+(sail==null ? '' : sail)+'天</span>'+(port==null ? '' : port)
+	+
+	'</div>';
+	if(index==0)
+		oDiv.innerHTML = oDiv.innerHTML+'<div class="mark-benefit"></div>';
+	else if(index==1)
+		oDiv.innerHTML = oDiv.innerHTML+'<div class="mark-fast"></div>';
+	oDiv.innerHTML = oDiv.innerHTML+
+
+	'<div class="item">'+
+	'<span>有效期：</span>'+
+	'<span>'+(expirydatebegin==null ? '' : expirydatebegin.replace('T',' ').substr(0,expirydatebegin.length-9))+'-'+(expirydateend==null ? '' : expirydateend.replace('T',' ').substr(0,expirydateend.length-9))+'</span>'+
+	'</div>'+
+	'<div class="item">'+
+	'<span>20GP/40GP/40HC：</span>'+
+	'<span>'+(GPprice20==null ? '' : GPprice20)+'/'+(GPprice40==null ? '' : GPprice40)+'/'+(HCprice40==null ? '' : HCprice40)+'</span>'+
+	'</div>'+
+	'<hr style="border:1px dashed #ebebeb;">'+
+	'<div class="item">'+
+	'<span>运费：</span>'+
+	'<span>'+TotalPrice+'</span>'+
+	'</div>'+
+	'</div>';	}
